74f7c449fafca9e51725def5660e62d91d4f1941
poly000 <34085039+poly000@users.noreply.github.com>
2021-02-07 13:31:22 +0800

    Update combine.sh

--- tags/combine.sh
+++ tags/combine.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-paste tags.txt tags_cn,txt glossary.txt
+echo '# vndb标签中英对照表' > glossary.md
+echo '-------------------------------  |-----------------------------' >> glossary.md
+rm pipe.txt;for a in {1..`wc tags.txt|gawk '{print $1}'`}                                                                                                                                                     ✔  1 task  13:28:04 
+do echo \| >> pipe.txt
+done
+paste tags.txt pipe.txt tags_cn,txt >> glossary.md
