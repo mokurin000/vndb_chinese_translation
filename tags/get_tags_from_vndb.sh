cc8df8f81c5fce95ae2e575ca12f532be70fca27
poly000 <1348292515@qq.com>
2021-02-07 16:38:27 +0800

    emmm

--- tags/get_tags_from_vndb.sh
+++ tags/get_tags_from_vndb.sh
@@ -2,7 +2,7 @@
 work_dir=`mktemp -td XXXXXXXXXX`
 current_dir="`pwd`"
 cd "${work_dir}"
-for i in {1..`curl https://vndb.org/g/list?a=1&t=2 | hq a data|grep last|head -n 1| sed 's!<a href="?o=a&amp;p=!!g;s!&amp;q=&amp;s=name">last »</a>!!g'`}
+for i in {1..`curl https://vndb.org/g/list'?b=1&a=1&t=2' | hq a data|grep last|head -n 1| sed 's!<a href="?a=1&amp;b=1&amp;o=a&amp;p=!!g;s!&amp;q=&amp;s=name&amp;t=2">last »</a>!!g'`}
 do  curl -O 'https://vndb.org/g/list?a=1&t=2&b=1&p='$i
     sleep 0.5
 done
