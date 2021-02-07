86cf46e65212c563ee940985a34a4772d68f638c
poly000 <1348292515@qq.com>
2021-02-07 15:02:32 +0800

    test

--- tags/combine.sh
+++ tags/combine.sh
@@ -3,9 +3,9 @@
 output_file=readme.md
 
 echo '# vndb标签中英对照表' > $output_file
-echo -e 'English 英文                     | Chinese 中文                  \n-------------------------------  |-----------------------------' >> $output_file
+echo -e 'English 英文                     | Chinese 中文                  |注释\n--------------------------------  |-------------------------------  |-----------------------------' >> $output_file
 rm pipe.txt
 for a in {1..`wc tags.txt|gawk '{print $1}'`} 
 do echo \| >> pipe.txt
 done
-paste tags.txt pipe.txt tags_cn.txt >> $output_file
+paste ./tags.txt ./pipe.txt ./tags_cn.txt ./pipe.txt ./tags_note.txt>> $output_file
