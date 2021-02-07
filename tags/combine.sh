4140043e6bce51d2385c13511fd66a6c1471bc03
poly000 <34085039+poly000@users.noreply.github.com>
2021-02-07 13:32:34 +0800

    Update combine.sh

--- tags/combine.sh
+++ tags/combine.sh
@@ -4,4 +4,4 @@ echo '-------------------------------  |-----------------------------' >> glossa
 rm pipe.txt;for a in {1..`wc tags.txt|gawk '{print $1}'`}                                                                                                                                                     ✔  1 task  13:28:04 
 do echo \| >> pipe.txt
 done
-paste tags.txt pipe.txt tags_cn,txt >> glossary.md
+paste tags.txt pipe.txt tags_cn.txt >> glossary.md
