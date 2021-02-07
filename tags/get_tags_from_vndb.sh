5d8920a7e75c1f5ce6d6711013b1a3910edcbfc2
poly000 <34085039+poly000@users.noreply.github.com>
2021-02-07 13:10:24 +0800

    Create get_tags_from_vndb.sh

---
+++ tags/get_tags_from_vndb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+work_dir=`mktemp -td XXXXXXXXXX`
+current_dir="`pwd`"
+cd "${work_dir}"
+for i in {1..`curl https://vndb.org/g/list | hq a data|grep last|head -n 1| sed 's!<a href="?o=a&amp;p=!!g;s!&amp;q=&amp;s=name">last »</a>!!g'`}
+do  curl -O 'https://vndb.org/g/list?p='$i
+    sleep 3
+done
+cat *|hq td.tc3 text|sort > "${current_dir}/tags.txt"
