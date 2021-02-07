9de7fb711ca38fac15a02c7dd983cf8d7b1ab8d0
poly000 <1348292515@qq.com>
2021-02-07 15:27:36 +0800

    emmmm

---
+++ tags/workdir/get_content_from_vndb.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+mkdir temp
+cd temp
+for i in {1..`curl https://vndb.org/g/list | hq a data|grep last|head -n 1| sed 's!<a href="?o=a&amp;p=!!g;s!&amp;q=&amp;s=name">last »</a>!!g'`}
+do  curl -O 'https://vndb.org/g/list?p='$i
+    sleep 3
+done
+
+cat *|hq td.tc3 text|grep -v deleted|grep -v ▴▾ > 0
+cat *|hq td.tc3 data|grep -v deleted|hq a attr href|grep -v o > 1
