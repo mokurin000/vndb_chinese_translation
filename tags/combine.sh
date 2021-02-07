eab55a93647ff550f268867948e157f28a3e48e2
poly000 <1348292515@qq.com>
2021-02-07 14:34:53 +0800

    fix

--- tags/combine.sh
+++ tags/combine.sh
@@ -3,7 +3,7 @@
 output_file=readme.md
 
 echo '# vndb标签中英对照表' > $output_file
-echo '-------------------------------  |-----------------------------' >> $output_file
+echo -e 'English 英文                     | Chinese 中文                  \n-------------------------------  |-----------------------------' >> $output_file
 rm pipe.txt
 for a in {1..`wc tags.txt|gawk '{print $1}'`} 
 do echo \| >> pipe.txt
